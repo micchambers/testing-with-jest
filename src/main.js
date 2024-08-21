3536eef11796a4c5cce15db56311804699c4aa0e
Micol <micolc76@gmail.com>
2024-08-21 17:50:14 -0400

    create mathStudents object, to be used as argument in fHSS function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+
+const mathStudents = [
+    { name: "Shane Carey", score: 9.5 },
+    { name: "Catarina Lima", score: 9.7 },
+    { name: "Rebecca Mills", score: 8.7 },
+  ];
+
 function findHighestScoringStudent(students) {
     // TODO
   }
