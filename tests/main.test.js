58bf67d6069f7378abd71cd1b0bcb016a7152b60
Micol <micolc76@gmail.com>
2024-08-21 18:02:54 -0400

    import fHSS function to test file and create empty test function

---
+++ tests/main.test.js
@@ -0,0 +1,7 @@
+const findHighestScoringStudent = require("../src/main");
+
+describe("findHighestScoringStudent", () => {
+    it("should return the student with the highest score", () => {
+    // Steps for testing the function
+    });
+});
